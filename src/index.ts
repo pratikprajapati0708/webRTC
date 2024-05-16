6c0fd0d844868fd853877db3de7c4640da28d5fa
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-16 22:17:21 +0530

    added message handlers for websocket and icecandidate

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+let senderSocket: null | WebSocket;
+let receiverSocket: null | WebSocket;
+
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
+  ws.on("message", function message(data:any) {
+    const message = JSON.parse(data);
+    if (message.type == "sender") {
+      senderSocket = ws;
+    } else if (message.type == "receiver") {
+      receiverSocket = ws;
+    } else if (message.type == "createOffer") {
+      if (ws !== senderSocket) {
+        return;
+      }
+      receiverSocket?.send(
+        JSON.stringify({ type: "createOffer", sdp: message.sdp })
+      );
+    } else if (message.type == "createAnswer") {
+      if (ws !== receiverSocket) {
+        return;
+      }
+      senderSocket?.send(
+        JSON.stringify({ type: "createAnswer", sdp: message.otp })
+      );
+    } else if (message.type === "iceCandidate") {
+      if (ws === senderSocket) {
+        receiverSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      } else if (ws === receiverSocket) {
+        senderSocket?.send(
+          JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+        );
+      }
+    }
   });
 
   ws.send("something");
