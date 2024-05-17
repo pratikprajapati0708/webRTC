8aeafb2be1632576ddf170a62e2064999b9c9612
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-17 12:59:00 +0530

    receiver code to let server know

---
+++ frontend/src/components/Receiver.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+export const Receiver = () => {
+    useEffect(() => {
+        const socket = new WebSocket("ws://localhost:8080");
+        socket.onopen = () => {
+            socket.send(JSON.stringify({ type: 'sender' }))
+        }
+    }, [])
+    return <div>
+
+    </div>
+}
