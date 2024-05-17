a71bffccbd9fd1269d36b8a6943cc40d9c3a613e
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-17 12:57:39 +0530

    sender code to let server know

--- frontend/src/components/Sender.tsx
+++ frontend/src/components/Sender.tsx
@@ -1,5 +1,12 @@
+import { useEffect } from "react"
 export const Sender = ()=>{
+    useEffect(()=>{
+        const socket = new WebSocket("ws://localhost:8080");
+        socket.onopen = ()=>{
+            socket.send(JSON.stringify({type : 'sender'}))
+        }
+    },[])
     return <div>
-        Sender
+        
     </div>
 }
