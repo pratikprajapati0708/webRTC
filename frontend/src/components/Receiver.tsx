d8c5fc9ca8ef151f71fee6fbaee16f44f6ee5385
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-17 13:23:31 +0530

    fix receiver type

--- frontend/src/components/Receiver.tsx
+++ frontend/src/components/Receiver.tsx
@@ -4,7 +4,7 @@ export const Receiver = () => {
     useEffect(() => {
         const socket = new WebSocket("ws://localhost:8080");
         socket.onopen = () => {
-            socket.send(JSON.stringify({ type: 'sender' }))
+            socket.send(JSON.stringify({ type: 'receiver' }))
         }
     }, [])
     return <div>
