4cdbaeda29b6778544a56e302a94aae5c4ca0052
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-17 13:06:55 +0530

    Sender code

--- frontend/src/components/Sender.tsx
+++ frontend/src/components/Sender.tsx
@@ -1,12 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 export const Sender = ()=>{
+
+    const[socket,setSocket] = useState<WebSocket | null>(null);
     useEffect(()=>{
         const socket = new WebSocket("ws://localhost:8080");
         socket.onopen = ()=>{
             socket.send(JSON.stringify({type : 'sender'}))
         }
     },[])
+    async function startSendingVideo(){
+        const pc = new RTCPeerConnection();
+        const offer = await pc.createOffer();
+        pc.setLocalDescription(offer);
+        socket?.send(JSON.stringify({type : 'createOffer', sdp : pc.localDescription }))
+    }
+
     return <div>
-        
+        <button onClick={startSendingVideo}>Sender</button>
     </div>
 }
