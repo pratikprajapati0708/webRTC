e86b17c8e3b760e1355badaf569fed9a609c57d7
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-20 11:49:56 +0530

    added receiver code in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Sender } from './components/Sender'
+import { Receiver } from './components/Receiver'
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/sender" element={<Sender/>} />
+          <Route path="/receiver" element={<Receiver/>} />
         </Routes>
       </BrowserRouter>
     </>
